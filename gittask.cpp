615e5ee769a9316af5f4ee1a18c76752080ed335
MaksimS4khnov <sahnoffmaksim@yandex.ru>
2022-03-17 13:00:22 +0300

    fibonacci function edit
    iteration output

--- gittask.cpp
+++ gittask.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
+#include <vector>
 
 
 using namespace std;
 
 int fib(int n){
-    if (n == 2 || n == 3){
-        return 1;
+    vector <int> mas;
+    mas.push_back(0);
+    mas.push_back(1);
+    int temp;
+    int count = 0;
+    int i =0;
+    while(count < n ){
+        if (count < n -1 ){
+            cout << mas[i] << endl;
+            temp = mas[i] + mas[i+1];
+            mas[i] = mas[i + 1];
+            mas[i+1] = temp;
+            count++;
+        }
+        else if(count == n - 1){
+            
+            break;
+        }
+        
     }
-    else{
-        return (fib(n-1) + fib(n-2));
-    }
+    return mas[i];
+
 }
 
 int main(){
