c47e2f93d231ea6cef67959956e3e2c4b12d51b0
MaksimS4khnov <sahnoffmaksim@yandex.ru>
2022-03-17 12:15:10 +0300

    Fibonacci function added

--- gittask.cpp
+++ gittask.cpp
@@ -3,8 +3,18 @@
 
 using namespace std;
 
+int fib(int n){
+    if (n == 1 || n == 2){
+        return 1;
+    }
+    else{
+        return (fib(n-1) + fib(n-2));
+    }
+}
+
 int main(){
 
     cout << "Hello, World!";
+    cout << fib(10);
     return 0;
 }
